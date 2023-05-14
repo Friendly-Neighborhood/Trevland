640312781ef89469cbb9a84502520f614662668c
Friendly-Neighborhood <agentspider12@gmail.com>
2023-05-15 01:29:33 +0300

    up

---
+++ js/slick.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $('.slider').slick();
+//     // $('.slider').slick({
+//     //     arrows: true,
+//     //     dots: false,
+//     //     adaptiveHeight: true,
+//     //     slidesToShow: 1,
+//     //     slidesToScroll: 1,
+//     //     speed: 300,
+//     //     easing: 'linear',
+//     //     infinite: true, 
+//     //     initialSlide: 0,
+//     //     autoplay: false, //автоплеер
+//     //     autoplaySpeed: 300,  // скорость при автоплее
+//     //     pauseOnDotsHover: false, //при автоплеере
+//     //     pauseOnHover: false,//при автоплеере
+//     //     pauseOnFocus: false,
+//     //     draggable: true, // свайп мышкой на десктопе
+//     //     swipe:true, // свайп на тачскрине
+//     //     touchThreshold: 1,
+//     //     centerMode: false, // центрировние при свайпе нескольких картинок
+//     //     variableWidth: false,
+//     //     rows: 1,
+//     //     slidesPerRow: 1,
+//     //     vertical: false, //вертикальный слайд
+//     //     verticalSwiping: false,
+//     //     // fade:true, // плавная замена при arrows:false
+        
+//     // });
+});
